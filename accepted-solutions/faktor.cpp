068aa8757af1e03983c1c231d3cb943bc66d5272
Lane Moseley <33134654+lanemoseley@users.noreply.github.com>
2019-02-26 09:31:51 -0700

    Add files via upload

---
+++ accepted-solutions/faktor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+   int a, i;
+   cin >> a;
+   cin >> i;
+
+   cout << (a * (i - 1)) + 1 << endl;
+
+   return 0;
+
+}
