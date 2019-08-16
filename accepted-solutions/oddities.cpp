d17255e66d67f4e1d5ed0849202398e7be0fe765
Lane Moseley <lane.moseley@mines.sdsmt.edu>
2019-08-16 08:42:40 -0600

    Refactored oddities.cpp

--- accepted-solutions/oddities.cpp
+++ accepted-solutions/oddities.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main()
 {
-    int num, temp;
-    vector<int> input;
+    int num, x;
 
     cin >> num;
+    
     do {
-        cin >> temp;
-        input.push_back(temp);
-    } while( --num > 0 );
-
-    for ( auto x : input )
-    {
+        cin >> x;
+        
         if ( x & 1 )
             cout << x << " is odd" << endl;
+        
         else
             cout << x << " is even" << endl;
-    }
+        
+        } while( --num > 0 );
 }
