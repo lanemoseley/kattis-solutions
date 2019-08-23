2410b82e318a3a45081e689af1daf69e72ac773f
Lane Moseley <lane.moseley@mines.sdsmt.edu>
2019-08-22 23:22:58 -0600

    Added python3 solution for aaah

---
+++ accepted-solutions/aaah/aaah.py
@@ -0,0 +1,11 @@
+def main():
+    jon = input()
+    doctor = input()
+
+    if len(jon) >= len(doctor):
+        print("go")
+    else:
+        print("no")
+
+if __name__ == "__main__":
+    main()
